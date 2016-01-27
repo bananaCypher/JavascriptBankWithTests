1d77acb21ed718494e32308dc5b0835cea4cb6a6
Callum Hogg <bananaCypher@gmail.com>
2016-01-27 17:48:58 +0000

    Dried up my test file

--- tests.js
+++ tests.js
@@ -2,6 +2,28 @@ var assert = require('assert');
 var Account = require ('./account');
 var Bank = require ('./bank');
 
+var testAccounts = function(){
+    return [
+        new Account('Callum', 200000, 'personal'),
+        new Account('John', 800000, 'personal'),
+        new Account('Iain', 20000, 'personal'),
+        new Account('Ryan', 4000000, 'business'),
+        new Account('Cameron', 2000000, 'business'),
+        new Account('Sarah', 1000000, 'business'),
+        new Account('Emma', 7400000, 'business'),
+        new Account('Julie', 3000000, 'business'),
+        new Account('Lorraine', 2000, 'personal'),
+        new Account('Samantha', 250000, 'personal'),
+    ]
+};
+var bankWithAccounts = function() {
+    var bank = new Bank();
+    for (account of testAccounts()) {
+        bank.addAccount(account);
+    } 
+    return bank;
+};
+
 describe('Account', function(){
     it('has account holder name', function(){
         var account = new Account('Callum', 200000, 'personal');
@@ -15,7 +37,7 @@ describe('Account', function(){
         var account = new Account('Callum', 200000, 'personal');
         assert.equal(200000, account.balance);
     });
-    it("balance can't be a float", function(){
+    it("balance can't be a float(balance is in pence)", function(){
         var account = new Account('Callum', 150.25, 'personal');
         assert.equal(true, account.invalid);
     });
@@ -49,174 +71,43 @@ describe('Bank', function(){
         assert.equal(1, bank.accounts.length);
     });
     it('can find account by name', function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
+        var accounts = testAccounts();
         assert.deepEqual(accounts[0], bank.findByName('Callum'));
     });
     it('can find account with wrong case', function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
+        var accounts = testAccounts();
         assert.deepEqual(accounts[3], bank.findByName('ryan'));
     });
     it("will return null if can't find account with name", function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
+        var accounts = testAccounts();
         assert.equal(null, bank.findByName('lolnope'));
     });
     it("will find the largest account", function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
+        var accounts = testAccounts();
         assert.deepEqual(accounts[6], bank.findLargestAccount());
     });
     it('will find the total balance of all accounts', function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
         assert.equal(186720.00, bank.findTotalBalance());
     });
     it('will find the average account balance of all accounts', function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
         assert.equal(18672.00, bank.findAverageBalance());
     });
     it('will find the total value for account type personal', function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
         assert.equal(12720.00, bank.findTotalBalanceOfAccountType('personal'));
     });
     it('will find the total value for account type business', function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
         assert.equal(174000.00, bank.findTotalBalanceOfAccountType('business'));
     });
     it("will return null if account type isn't personal or business", function(){
-        var bank = new Bank();
-        var accounts = [
-            new Account('Callum', 200000, 'personal'),
-            new Account('John', 800000, 'personal'),
-            new Account('Iain', 20000, 'personal'),
-            new Account('Ryan', 4000000, 'business'),
-            new Account('Cameron', 2000000, 'business'),
-            new Account('Sarah', 1000000, 'business'),
-            new Account('Emma', 7400000, 'business'),
-            new Account('Julie', 3000000, 'business'),
-            new Account('Lorraine', 2000, 'personal'),
-            new Account('Samantha', 250000, 'personal'),
-        ]
-        for (account of accounts) {
-            bank.addAccount(account);
-        } 
+        var bank = bankWithAccounts();
         assert.equal(null, bank.findTotalBalanceOfAccountType('testing123'));
     });
 });
